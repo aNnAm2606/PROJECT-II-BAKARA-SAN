e2f5b8bbf029ec5535463740b9aa5c940d827aad
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2022-04-18 22:48:05 +0200

    Implemented Gamepad and Haptic Support

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -56,7 +56,8 @@ bool Scene::Update(float dt)
 	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
 		app->render->camera.x += 1;
 
-	//app->render->DrawTexture(img, 380, 100);
+	app->render->DrawTexture(img, 380, 100);
+
 
 	return true;
 }
